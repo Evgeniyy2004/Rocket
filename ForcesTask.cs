f7fcec0738350ea214ecde30de311248cffd462e
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-07-24 00:23:12 +0500

    С Y беда

--- ForcesTask.cs
+++ ForcesTask.cs
@@ -5,29 +5,31 @@ namespace func_rocket;
 
 public class ForcesTask
 {
-	/// <summary>
-	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
-	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
-	/// </summary>
-	public static RocketForce GetThrustForce(double forceValue)
-	{
-		return x => new Vector(Math.Cos(x.Direction)*forceValue,Math.Sin(x.Direction)*forceValue);
+    /// <summary>
+    /// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+    /// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+    /// </summary>
+    public static RocketForce GetThrustForce(double forceValue)
+    {
+        return x => new Vector(Math.Cos(x.Direction) * forceValue, Math.Sin(x.Direction) * forceValue);
     }
 
-	/// <summary>
-	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
-	/// </summary>
-	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize)
-	{
-		return x => gravity(spaceSize, x.Location);
-	}
+    /// <summary>
+    /// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+    /// </summary>
+    public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize)
+    {
+        return x => gravity(spaceSize, x.Location);
+    }
 
-	/// <summary>
-	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
-	/// </summary>
-	public static RocketForce Sum(params RocketForce[] forces)
-	{
-		return x =>new Vector(forces.ToList().Select(r => r(x)).Select(p=>p.X).Sum(), forces.ToList().Select(r => r(x)).Select(p => p.Y).Sum());
-	
-	}
+    /// <summary>
+    /// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
+    /// </summary>
+    public static RocketForce Sum(params RocketForce[] forces)
+    {
+        return x => new Vector(forces.ToList().Select(r => r(x))
+                              .Select(p => p.X)
+                              .Sum(), forces.ToList()
+                                .Select(r => r(x)).Select(p => p.Y).Sum());
+    }
 }
--- LevelsTask.cs
+++ LevelsTask.cs
@@ -13,6 +13,27 @@ public class LevelsTask
 			new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
 			new Vector(600, 200), 
 			(size, v) => Vector.Zero, standardPhysics);
-		//TODO: ...
-	}
+		yield return new Level("Heavy",
+            new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
+            new Vector(600, 200),
+            (size, v) => new Vector(0,-0.9), standardPhysics);
+        yield return new Level("Up",
+            new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
+            new Vector(700, 500),
+            (size, v) => new Vector(0, 300 / (v.Y + 300.0)), standardPhysics);
+        yield return new Level("WhiteHole",
+            new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
+            new Vector(600, 200),
+            (size, v) => new Vector(140 * (v.X-200) / ((v.X - 200) * (v.X - 200) + 1), 140 * (v.Y - 200) / ((v.Y - 200) * (v.Y - 200) + 1)), standardPhysics);
+        yield return new Level("BlackHole",
+            new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
+            new Vector(600, 200),
+            (size, v) => new Vector(300 * (400 - v.X) / ((400-v.X) * (400-v.X) + 1), 300 * (350-v.Y) / ((350-v.Y) * (350-v.Y) + 1)), standardPhysics);
+        yield return new Level("BlackAndWhite",
+            new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
+            new Vector(600, 200),
+            (size, v) => new Vector(300 * (400 - v.X) / ((400 - v.X) * (400 - v.X) + 1)*0.5+ 140 * (v.X - 200) / ((v.X - 200) * (v.X - 200) + 1)*0.5, 140 * (v.Y - 200) / ((v.Y - 200) * (v.Y - 200) + 1)*0.5+ 300 * (350 - v.Y) / ((350 - v.Y) * (350 - v.Y) + 1)*0.5), standardPhysics);
+
+        //TODO: ...
+    }
 }
