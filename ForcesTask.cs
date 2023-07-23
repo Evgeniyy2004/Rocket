5a0932a56cd7a0c74e228c4730273d1ef4688986
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-07-23 18:16:45 +0500

    Добавьте файлы проекта.

---
+++ ForcesTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace func_rocket;
+
+public class ForcesTask
+{
+	/// <summary>
+	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+	/// </summary>
+	public static RocketForce GetThrustForce(double forceValue)
+	{
+		return x=>new Vector(x.Velocity.X/Math.Abs(x.Velocity.X), x.Velocity.Y / Math.Abs(x.Velocity.Y))
+    }
+
+	/// <summary>
+	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+	/// </summary>
+	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize)
+	{
+		return x => gravity(spaceSize, x.Location);
+	}
+
+	/// <summary>
+	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
+	/// </summary>
+	public static RocketForce Sum(params RocketForce[] forces)
+	{
+		return x =>new Vector(forces.ToList().Select(r => r(x)).Select(p=>p.X).Sum(), forces.ToList().Select(r => r(x)).Select(p => p.Y).Sum());
+	
+	}
+}
