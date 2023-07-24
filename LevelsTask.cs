cb2bc2459cba685e2d3e77615f6dd5dd2ec16b42
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-07-24 15:21:07 +0500

    Ужасно, но работает

--- LevelsTask.cs
+++ LevelsTask.cs
@@ -24,7 +24,7 @@ public class LevelsTask
         yield return new Level("WhiteHole",
             new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
             new Vector(600, 200),
-            (size, w) => new Vector(140 * (w.X-600) / ((w.X - 600) * (w.X - 600) + 1), 140 * (w.Y - 200) / ((w.Y - 200) * (w.Y - 200) + 1)), standardPhysics);
+            (size, w) => new Vector(Math.Cos(Math.Atan2(w.Y-200,w.X-600))*140 * Math.Sqrt((w.Y - 200) * (w.Y - 200) + (w.X - 600) * (w.X - 600)) / ((w.Y - 200) * (w.Y - 200) + (w.X - 600) * (w.X - 600) + 1), Math.Sin(Math.Atan2(w.Y - 200, w.X - 600)) * 140 * Math.Sqrt((w.Y - 200) * (w.Y - 200) + (w.X - 600) * (w.X - 600)) / ((w.Y - 200) * (w.Y - 200) + (w.X - 600) * (w.X - 600) + 1)), standardPhysics);
         yield return new Level("BlackHole",
             new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
             new Vector(600, 200),
@@ -32,7 +32,7 @@ public class LevelsTask
         yield return new Level("BlackAndWhite",
             new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
             new Vector(600, 200),
-            (size, f) => new Vector(300 * (400 - f.X) / ((400 - f.X) * (400 - f.X) + 1)*0.5+ 140 * (f.X - 600) / ((f.X - 600) * (f.X - 600) + 1)*0.5, 140 * (f.Y - 200) / ((f.Y - 200) * (f.Y - 200) + 1)*0.5+ 300 * (350 - f.Y) / ((350 - f.Y) * (350 - f.Y) + 1)*0.5), standardPhysics);
+            (size, f) => new Vector(Math.Cos(Math.Atan2(f.Y - 200, f.X - 600)) * 140 * Math.Sqrt((f.Y - 200) * (f.Y - 200) + (f.X - 600) * (f.X - 600)) / ((f.Y - 200) * (f.Y - 200) + (f.X - 600) * (f.X - 600) + 1)/2+ Math.Cos(Math.Atan2(350 - f.Y, 400 - f.X)) * 300 * Math.Sqrt((400 - f.X) * (400 - f.X) + (350 - f.Y) * (350 - f.Y)) / (((400 - f.X) * (400 - f.X) + (350 - f.Y) * (350 - f.Y)) + 1)/2, Math.Sin(Math.Atan2(f.Y - 200, f.X - 600)) * 140 * Math.Sqrt((f.Y - 200) * (f.Y - 200) + (f.X - 600) * (f.X - 600)) / ((f.Y - 200) * (f.Y - 200) + (f.X - 600) * (f.X - 600) + 1) / 2 + Math.Sin(Math.Atan2(350 - f.Y, 400 - f.X)) * 300 * Math.Sqrt((400 - f.X) * (400 - f.X) + (350 - f.Y) * (350 - f.Y)) / (((400 - f.X) * (400 - f.X) + (350 - f.Y) * (350 - f.Y)) + 1) / 2), standardPhysics);
 
         //TODO: ...
     }
