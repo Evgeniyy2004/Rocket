ae97d7dfd9bffde83915610d902732fe22495dab
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-07-25 22:56:00 +0500

    Done вроде

--- ControlTask.cs
+++ ControlTask.cs
@@ -1,9 +1,25 @@
+using System;
+
 namespace func_rocket;
 
 public class ControlTask
 {
-	public static Turn ControlRocket(Rocket rocket, Vector target)
-	{
-		return Turn.None;
-	}
+    public static Turn ControlRocket(Rocket rocket, Vector target)
+    {
+        
+        var c = target - rocket.Location;
+        var angle11 = c.Angle - rocket.Velocity.Angle;
+        var angle2 = c.Angle - rocket.Direction;
+        double resultangle;
+        if (Math.Abs(angle11) > 0.6 && Math.Abs(angle2) > 0.6)
+        {
+            resultangle = angle2;
+        }
+        else resultangle = angle11 + angle2;
+
+        if (resultangle > 0) return Turn.Right;
+        if (resultangle < 0) return Turn.Left;
+        return Turn.None;
+
+    }
 }
